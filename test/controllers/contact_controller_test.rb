ddf2bce1184d51109b19f61edad32001b953666a
Rio <rv00138@surrey.ac.uk>
2021-01-03 16:45:19 +0000

    Added contact form

--- test/controllers/contact_controller_test.rb
+++ test/controllers/contact_controller_test.rb
@@ -12,4 +12,20 @@ class ContactControllerTest < ActionDispatch::IntegrationTest
     assert_select 'p', 'Complete the form if you have any queries.'
   end
 
+  test "should post request contact but no email" do
+    post request_contact_url
+
+    assert_response :redirect
+    assert_not_empty flash[:alert]
+    assert_nil flash[:notice]
+  end
+
+  test "should post request contact" do
+    post request_contact_url, params:
+      {name: "Matthew", email: "matthew@email.com", telephone: "12345678901", message: "Hello"}
+      assert_response :redirect
+      assert_nil flash[:alert]
+      assert_not_empty flash[:notice]
+  end
+
 end
